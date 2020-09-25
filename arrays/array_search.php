f955e72f741b4a9c5abbe840be80618156a2dd07
Anatolii <developer@languages-with-music.com>
2020-09-25 13:35:34 +0200

    add using echo

--- arrays/array_search.php
+++ arrays/array_search.php
@@ -2,4 +2,6 @@
 $array = [0 => 'blue', 1 => 'red', 2 => 'green', 3 => 'red'];
 
 $key = array_search('green', $array); // $key = 2;
+echo $key, '<br />';
 $key = array_search('red', $array);   // $key = 1;
+echo $key, '<br />';
