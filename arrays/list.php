b30308bc30b93e056d8fd173ddefdfd8bf3007e9
Anatolii <developer@languages-with-music.com>
2020-09-25 11:39:54 +0200

    add <br />

--- arrays/list.php
+++ arrays/list.php
@@ -1,6 +1,6 @@
 <?php
 $arr = [1, 2, 3];
 list($one, $two, $three) = $arr;
-echo $one;   // 1
-echo $two;   // 2
-echo $three; // 3
+echo $one . '<br />';   // 1
+echo $two . '<br />';   // 2
+echo $three . '<br />'; // 3
