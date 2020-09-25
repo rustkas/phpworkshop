bcffb4baba919007fcfb87f728962be51bf27ae5
Anatolii <developer@languages-with-music.com>
2020-09-25 22:19:12 +0200

    init $number variable

--- strings/str_replace_count.php
+++ strings/str_replace_count.php
@@ -1,5 +1,6 @@
 <?php
 $str = '[b]Это[/b] очень жирный [b]текст[/b].';
+$number = 0;
 echo str_replace(['[b]', '[/b]'], ['', ''], $str, $number);
 echo '<br />';
 echo "Осуществлено замен: $number";
